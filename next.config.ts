b491534f06a3b9415d82d97ad43fe5e581cb3c36
kshetritej <tejghartikshetri@gmail.com>
2026-02-22 16:52:54 +0545

    chore: update link in next config

--- next.config.ts
+++ next.config.ts
@@ -6,26 +6,26 @@ const nextConfig: NextConfig = {
       return [
         {
           source: "/api/:path*",
-          destination: "https://api.growfore.com/api/:path*",
+          destination: "https://api.summitluxurytreks.com/api/:path*",
         },
       ];
     }
-    
+
     return [];
   },
-  
+
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/api/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/api/**",
       },
       {
-        protocol: 'https',
-        hostname: 'api.growfore.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "api.summitluxurytreks.com",
+        pathname: "/**",
       },
     ],
   },
